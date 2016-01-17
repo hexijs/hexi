234cd772e6cb8bc651bbda6a5ae0982a209f567b
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-01-17 03:01:30 +0200

    tasks can be executed before handlers

--- example/index.js
+++ example/index.js
@@ -5,9 +5,20 @@ let server = new hexi.Server()
 
 server.connection(3000)
 
+server.task('foo', ['bar'], (req, res, next) => {
+  console.log('foo')
+  next()
+})
+
+server.task('bar', (req, res, next) => {
+  console.log('bar')
+  next()
+})
+
 server.route({
   method: 'GET',
   path: '/',
+  task: ['foo'],
   handler(req, res) {
     res.send('Hello world!')
   },
