f7473a7d26641198e500d889b654466d2e50d7f6
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-02-13 15:27:46 +0200

    tests structure updated

--- test/index.js
+++ test/index.js
@@ -8,97 +8,20 @@ const sinon = require('sinon')
 
 chai.use(require('sinon-chai'))
 
-describe('server route', function() {
+describe('hexi', function() {
   let server
 
   beforeEach(function() {
     server = hexi()
   })
 
-  it('should execute handler', function(done) {
-    server.route({
-      method: 'GET',
-      path: '/foo',
-      handler(req, res) {
-        res.send('bar')
-      },
-    })
-
-    request(server.express)
-      .get('/foo')
-      .expect(200, 'bar', done)
-  })
-
-  it('should execute listen to all method requests passed', function(done) {
-    server.route({
-      method: ['GET', 'POST'],
-      path: '/foo',
-      handler(req, res) {
-        res.send('bar')
-      },
-    })
-
-    request(server.express)
-      .get('/foo')
-      .expect(200, 'bar', function(err) {
-        if (err) return done(err)
-
-        request(server.express)
-          .post('/foo')
-          .expect(200, 'bar', done)
-      })
-  })
-
-  it('should execute several handlers', function(done) {
-    const handler1 = sinon.spy((req, res, next) => next())
-
-    server.route({
-      method: 'GET',
-      path: '/foo',
-      task: ['task'],
-      handler: [
-        handler1,
-        (req, res) => {
-          expect(handler1).to.have.been.calledOnce
-          res.send('bar')
-        },
-      ],
-    })
-
-    request(server.express)
-      .get('/foo')
-      .expect(200, 'bar', done)
-  })
-
-  it('should pass route options to handler', function(done) {
-    server.route({
-      method: 'GET',
-      path: '/foo',
-      handler(req, res) {
-        expect(req.route).to.exist
-        res.send('bar')
-      },
-    })
-
-    request(server.express)
-      .get('/foo')
-      .expect(200, 'bar', done)
-  })
-
-  describe('pre', function() {
-    it('should exec pre hook', function(done) {
-      server.route.pre(function(next, opts) {
-        opts.config = { foo: 'bar' }
-        expect(opts.pre).to.be.instanceof(Array)
-        expect(opts.pre.length).to.eq(1)
-        next(opts)
-      })
-
+  describe('route', function() {
+    it('should execute handler', function(done) {
       server.route({
         method: 'GET',
         path: '/foo',
-        pre(req, res) {
-          res.send(req.route.config.foo)
+        handler(req, res) {
+          res.send('bar')
         },
       })
 
@@ -107,26 +30,97 @@ describe('server route', function() {
         .expect(200, 'bar', done)
     })
 
-    it('should path empty array to route pre hook when no handler passed', function(done) {
-      server.route.pre(function(next, opts) {
-        expect(opts.pre).to.be.instanceof(Array)
-        expect(opts.pre.length).to.eq(0)
-        done()
+    it('should execute listen to all method requests passed', function(done) {
+      server.route({
+        method: ['GET', 'POST'],
+        path: '/foo',
+        handler(req, res) {
+          res.send('bar')
+        },
       })
+
+      request(server.express)
+        .get('/foo')
+        .expect(200, 'bar', function(err) {
+          if (err) return done(err)
+
+          request(server.express)
+            .post('/foo')
+            .expect(200, 'bar', done)
+        })
+    })
+
+    it('should execute several handlers', function(done) {
+      const handler1 = sinon.spy((req, res, next) => next())
 
       server.route({
         method: 'GET',
         path: '/foo',
+        task: ['task'],
+        handler: [
+          handler1,
+          (req, res) => {
+            expect(handler1).to.have.been.calledOnce
+            res.send('bar')
+          },
+        ],
+      })
+
+      request(server.express)
+        .get('/foo')
+        .expect(200, 'bar', done)
+    })
+
+    it('should pass route options to handler', function(done) {
+      server.route({
+        method: 'GET',
+        path: '/foo',
+        handler(req, res) {
+          expect(req.route).to.exist
+          res.send('bar')
+        },
+      })
+
+      request(server.express)
+        .get('/foo')
+        .expect(200, 'bar', done)
+    })
+
+    describe('pre', function() {
+      it('should exec pre hook', function(done) {
+        server.route.pre(function(next, opts) {
+          opts.config = { foo: 'bar' }
+          expect(opts.pre).to.be.instanceof(Array)
+          expect(opts.pre.length).to.eq(1)
+          next(opts)
+        })
+
+        server.route({
+          method: 'GET',
+          path: '/foo',
+          pre(req, res) {
+            res.send(req.route.config.foo)
+          },
+        })
+
+        request(server.express)
+          .get('/foo')
+          .expect(200, 'bar', done)
+      })
+
+      it('should path empty array to route pre hook when no handler passed', function(done) {
+        server.route.pre(function(next, opts) {
+          expect(opts.pre).to.be.instanceof(Array)
+          expect(opts.pre.length).to.eq(0)
+          done()
+        })
+
+        server.route({
+          method: 'GET',
+          path: '/foo',
+        })
       })
     })
-  })
-})
-
-describe('server', function() {
-  let server
-
-  beforeEach(function() {
-    server = hexi()
   })
 
   it('should have isHexi property', function() {
